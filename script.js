f08d26c707b3e85a8ffb1223c72470823c120c06
Danny Befumo <DPBefumo@gmail.com>
2020-05-05 15:47:17 -0400

    Initial set up with HTML,CSS, & JS

---
+++ script.js
@@ -0,0 +1,20 @@
+// will need to gather the following info from customers:
+// name
+// address
+// phone number or email
+// tumbnail
+// birthdate
+// registered date
+
+//tumbnail (ID = "customer-image") from js (picture.thumbnail)
+//name (ID = "customer-name") from js (name.first, name.last)
+//address (ID = "customer-address") from js (location.street, lovation.city, location.state, location.postcode)
+//phone number or email (ID = "customer-email") from js (phone or email)
+//birthdate (ID = "customer-DOB") from js (dob)
+//registered (ID = "customer-since") from js (registered)
+
+function renderPageContent() {
+    console.log("Make this page")
+}
+
+renderPageContent ()
